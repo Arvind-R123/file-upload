ba86d0b33ecf9c223c2cb979172d0e34c6856e49
Arvind-R123 <aravindhrajangam123@gmail.com>
2025-03-25 21:26:54 -0700

    file.php

--- file.php
+++ file.php
@@ -21,13 +21,13 @@ if (isset($_POST["submit"])) {
 
 
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
+  echo "File is already exists.";
   $uploadOk = 0;
 }
 
 
 if ($_FILES["newfile"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
+  echo "Your file is too large.";
   $uploadOk = 0;
 }
 
@@ -36,13 +36,13 @@ if (
   $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
   && $imageFileType != "gif"
 ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  echo "Only JPG, JPEG, PNG & GIF files are allowed.";
   $uploadOk = 0;
 }
 
 
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
+  echo "Your file was not uploaded.";
 } else {
   if (move_uploaded_file($_FILES["newfile"]["tmp_name"], $target_file)) {
     echo "The file " . htmlspecialchars(basename($_FILES["newfile"]["name"])) /*. " has been uploaded."*/ . header("Location:./Welcome.html");;
