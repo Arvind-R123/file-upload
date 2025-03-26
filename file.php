bb24d447bcbad19abe7e3c56068a2807704c9df3
Arvind-R123 <aravindhrajangam123@gmail.com>
2025-03-25 21:30:14 -0700

    file.php

--- file.php
+++ file.php
@@ -1,7 +1,7 @@
 <?php
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["newfile"]["name"]);
-$uploadOk = 1;
+$upload = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 
@@ -11,24 +11,24 @@ if (isset($_POST["submit"])) {
 
   if ($check !== false) {
     echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
+    $upload = 1;
 
   } else {
     echo "File is not an image.";
-    $uploadOk = 0;
+    $upload = 0;
   }
 }
 
 
 if (file_exists($target_file)) {
   echo "File is already exists.";
-  $uploadOk = 0;
+  $upload = 0;
 }
 
 
 if ($_FILES["newfile"]["size"] > 500000) {
   echo "Your file is too large.";
-  $uploadOk = 0;
+  $upload = 0;
 }
 
 
@@ -37,16 +37,16 @@ if (
   && $imageFileType != "gif"
 ) {
   echo "Only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
+  $upload = 0;
 }
 
 
-if ($uploadOk == 0) {
+if ($upload == 0) {
   echo "Your file was not uploaded.";
 } else {
   if (move_uploaded_file($_FILES["newfile"]["tmp_name"], $target_file)) {
     echo "The file " . htmlspecialchars(basename($_FILES["newfile"]["name"])) /*. " has been uploaded."*/ . header("Location:./Welcome.html");;
   } else {
-    echo "Sorry, there was an error uploading your file.";
+    echo "Not uploading your file.";
   }
 }
